6cc1ac0b0d4a45c0e7e2b435908ff336c68ecf9f
Alex <webme.co@yandex.ru>
2023-03-03 21:05:36 +0300

    hotfix

--- framework/config.js
+++ framework/config.js
@@ -4,6 +4,8 @@ const config = {
     userName: "Jean-Jacques.Rousseau",
     password: "HNc8T4x3TMAnchZ3!",
   },
+  userid: "0492d0c2-3a21-4aa4-8e72-23b4c567fc2c",
+  isbn: "9781491904244",
 };
 
 export default config;
--- specs/index.spec.js
+++ specs/index.spec.js
@@ -1,4 +1,36 @@
 import user from "../framework/services/user";
 import config from "../framework/config";
 
-describe("ADD BOOKS TO USER", () => {});
+describe("ADD BOOKS TO USER", () => {
+  test("Запрос на добавление книги возвращает 201 и массив с книгой", async () => {
+    const token = await user.getToken();
+    const response = await user.addBooks(token);
+    expect(response.status).toEqual(201);
+    expect(response.body).toEqual({
+      books: [{ isbn: config.isbn }],
+    });
+  });
+
+  test("Запрос на добавление книги возвращает 400 и код ошибки 1210, если книга есть в коллекции", async () => {
+    const token = await user.getToken();
+    const response = await user.addBooks(token);
+    expect(response.status).toEqual(400);
+    expect(response.body.code).toEqual("1210");
+    expect(response.body.message).toEqual(
+      "ISBN already present in the User's Collection!"
+    );
+  });
+
+  test("Запрос на добавление книги возвращает 401 и код ошибки 1200, если юзер не авторизован", async () => {
+    const response = await user.addBooks(null);
+    expect(response.status).toEqual(401);
+    expect(response.body.code).toEqual("1200");
+    expect(response.body.message).toEqual("User not authorized!");
+  });
+
+  test("All books have been deleted from the user collection", async () => {
+    const token = await user.getToken();
+    const response = await user.deleteBooks(token);
+    expect(response.status).toEqual(204);
+  });
+});
